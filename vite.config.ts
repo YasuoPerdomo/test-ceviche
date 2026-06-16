6f95d3696a7ed9d058fac6021d20748e0fd2eefd
YasuoPerdomo <50577446+YasuoPerdomo@users.noreply.github.com>
2026-06-15 22:52:42 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "./",
+    base: "/test-ceviche/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
